a845cec439a54d49a4507176dd1f7252f82239ac
hidayat00 <135511484+hidayat00@users.noreply.github.com>
2024-03-13 16:57:24 +0800

    Add files via upload

---
+++ Tugas1_SourceCode_Praktek/scr/incr.java
@@ -0,0 +1,13 @@
+/*
+Nim  : 13020220088
+Nama : Muh. Nur Hidayat
+--Sabtu, 21/02/2024--
+ */
+public class incr {
+    public static void main(String[] args) {
+        int i, j;
+        i = 3;
+        j = i++;
+        System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+    }
+}
